6726cd0a6875fd1eba1640f747c9fff58d346db5
Chris <46053520+Sturm117@users.noreply.github.com>
2019-01-10 15:40:23 -0600

    Update BarrenLandAnalysis.java

--- BarrenLandAnalysis.java
+++ BarrenLandAnalysis.java
@@ -1,16 +1,13 @@
 
 public class BarrenLandAnalysis {
+	BLA_Farmland defaultFarm = new BLA_Farmland(399,599);
 
 	public static void main() {
-		BLA_Farmland defaultFarm = new BLA_Farmland(399,599);
-		
 		defaultFarm.getBarrenLandCoordinates(null);
 		defaultFarm.printConnectedFertileArea();
 	}
 	
-	public static void main(String[] args) {
-		BLA_Farmland defaultFarm = new BLA_Farmland(399,599);
-		
+	public static void main(String[] args) {	
 		defaultFarm.getBarrenLandCoordinates(args[0]);
 		defaultFarm.printConnectedFertileArea();
 	}
